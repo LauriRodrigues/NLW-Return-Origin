3e678f1e59c765efdff755f86a3693407137c348
Lauri <laurianarodrigueslima@gmail.com>
2022-05-05 21:29:20 -0300

    Dia 4

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
 function onScroll() {
+  showNavOnScroll ()
+  showBackToTopButtonOnScroll()
+}
+ 
+function showNavOnScroll () {
   if (scrollY>0) {
     navigation.classList.add('scroll') // para add a class scroll na tag nav
   } else 
       navigation.classList.remove('scroll') // para remover a class scroll na tag nav
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY>550) {
+    backToTopButton.classList.add('show') // para add a class scroll na tag nav
+  } else 
+      backToTopButton.classList.remove('show') // para remover a class scroll na tag nav
 }
 
 function openMenu () {
