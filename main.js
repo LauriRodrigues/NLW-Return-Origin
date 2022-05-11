cf93ab291fbc5d640cbaec4230ac16c8cc0223a4
Lauri <laurianarodrigueslima@gmail.com>
2022-05-10 21:31:50 -0300

    Dia 5

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ onScroll()
 function onScroll() {
   showNavOnScroll ()
   showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
- 
+
 function showNavOnScroll () {
   if (scrollY>0) {
     navigation.classList.add('scroll') // para add a class scroll na tag nav
@@ -19,6 +23,28 @@ function showBackToTopButtonOnScroll() {
     backToTopButton.classList.add('show') // para add a class scroll na tag nav
   } else 
       backToTopButton.classList.remove('show') // para remover a class scroll na tag nav
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function openMenu () {
